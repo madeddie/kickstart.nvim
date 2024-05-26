e336fcba9076ddd3e5d70eb8729e4e14e778e1c7
Edwin Hermans <edwin.hermans@curology.com>
2024-05-26 14:13:36 -0400

    Testrun ruff (server) as lsp

--- init.lua
+++ init.lua
@@ -578,7 +578,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        -- pyright = {},
+        -- pylsp = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
